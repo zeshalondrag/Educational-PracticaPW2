42efc7c7b0adcd4c9d9ab44e24872ad4d371aa1e
zeshalondrag <144255214+zeshalondrag@users.noreply.github.com>
2024-04-03 01:24:22 +0300

    Dealership.sql

--- Dealership.sql
+++ Dealership.sql
@@ -8,7 +8,7 @@ CREATE TABLE Customers(
 	ID_Customer INT PRIMARY KEY IDENTITY(1,1),
 	CustomerSurname VARCHAR(50) NOT NULL,
 	CustomerName VARCHAR(50) NOT NULL,
-	CustomerMiddleName VARCHAR(50),
+	CustomerMiddleName VARCHAR(50) NOT NULL,
 	PhoneNumber VARCHAR(11) NOT NULL UNIQUE,
 	Email VARCHAR(100) NOT NULL UNIQUE
 );
@@ -16,11 +16,26 @@ GO
 
 INSERT INTO Customers(CustomerSurname, CustomerName, CustomerMiddleName, PhoneNumber, Email)
 VALUES
-	('Васильева', 'Ариана', 'Евгеньева', '79338729980', 'vasileva@mail.ru'),
-	('Макарова', 'Ксения', 'Данииловна', '79064423361', 'makarov@gmail.com'),
-	('Леонов', 'Степан', 'Александрович', '79309033880', 'leonov@ya.ru'),
-	('Сизова', 'София', 'Богдановна', '79641567792', 'sizova@mail.ru'),
-	('Комаров', 'Данила', 'Иванович', '79316925523', 'komarov@ya.ru`');
+	('Р’Р°СЃРёР»СЊРµРІР°', 'РђСЂРёР°РЅР°', 'Р•РІРіРµРЅСЊРµРІР°', '79338729980', 'vasileva@mail.ru'),
+	('РњР°РєР°СЂРѕРІР°', 'РљСЃРµРЅРёСЏ', 'Р”Р°РЅРёРёР»РѕРІРЅР°', '79064423361', 'makarov@gmail.com'),
+	('Р›РµРѕРЅРѕРІ', 'РЎС‚РµРїР°РЅ', 'РђР»РµРєСЃР°РЅРґСЂРѕРІРёС‡', '79309033880', 'leonov@ya.ru'),
+	('РЎРёР·РѕРІР°', 'РЎРѕС„РёСЏ', 'Р‘РѕРіРґР°РЅРѕРІРЅР°', '79641567792', 'sizova@mail.ru'),
+	('РљРѕРјР°СЂРѕРІ', 'Р”Р°РЅРёР»Р°', 'РРІР°РЅРѕРІРёС‡', '79316925523', 'komarov@ya.ru');
+GO
+
+CREATE TABLE Brands(
+	ID_Brand INT PRIMARY KEY IDENTITY(1,1),
+	BrandName VARCHAR(50) NOT NULL UNIQUE
+);
+GO
+
+INSERT INTO Brands(BrandName)
+VALUES
+	('Ferrari'),
+	('Audi'),
+	('Mercedes-Benz'),
+	('Porsche'),
+	('Volkswagen');
 GO
 
 CREATE TABLE Models(
@@ -38,20 +53,6 @@ VALUES
 	('Tiguan');
 GO
 
-CREATE TABLE Brands(
-	ID_Brand INT PRIMARY KEY IDENTITY(1,1),
-	BrandName VARCHAR(50) NOT NULL UNIQUE
-);
-GO
-
-INSERT INTO Brands(BrandName)
-VALUES
-	('Ferrari'),
-	('Audi'),
-	('Mercedes-Benz'),
-	('Porsche'),
-	('Volkswagen');
-GO
 
 CREATE TABLE Vehicles(
 	ID_Vehicle INT PRIMARY KEY IDENTITY(1,1),
@@ -75,22 +76,45 @@ CREATE TABLE Orders(
 	ID_Order INT PRIMARY KEY IDENTITY(1,1),
 	Customer_ID INT NOT NULL FOREIGN KEY REFERENCES Customers(ID_Customer),
 	Vehicle_ID INT NOT NULL FOREIGN KEY REFERENCES Vehicles(ID_Vehicle),
-	OrderDate DATE NOT NULL,
+	OrderDate VARCHAR(10) NOT NULL,
 	Amount INT NOT NULL
 );
 GO
 
 INSERT INTO Orders(Customer_ID, Vehicle_ID, OrderDate, Amount)
 VALUES
-    (1, 1, '27-02-2024', 1),
-    (2, 3, '10-06-2023', 2),
-    (3, 2, '28-08-2023', 1),
-    (4, 4, '26-10-2023', 1),
-    (5, 5, '15-06-2023', 3);
+    (1, 1, '27.02.2024', 1),
+    (2, 2, '10.06.2021', 5),
+    (3, 3, '28.08.2023', 1),
+    (4, 4, '26.10.2022', 2),
+    (5, 5, '15.06.2023', 4);
 GO
 
 SELECT * FROM Customers
-SELECT * FROM Models
-SELECT * FROM Brands
 SELECT * FROM Vehicles
+SELECT * FROM Brands
+SELECT * FROM Models
 SELECT * FROM Orders
+
+SELECT 
+    Customers.ID_Customer,
+    Customers.CustomerSurname, 
+    Customers.CustomerName, 
+    Customers.CustomerMiddleName, 
+    Customers.PhoneNumber, 
+    Customers.Email, 
+    Vehicles.ID_Vehicle,
+    Brands.ID_Brand,
+    Brands.BrandName, 
+    Models.ID_Model,
+    Models.ModelName, 
+    Vehicles.Years, 
+    Vehicles.Price, 
+    Orders.ID_Order,
+    Orders.OrderDate, 
+    Orders.Amount
+FROM Orders
+INNER JOIN Customers ON Orders.Customer_ID = Customers.ID_Customer
+INNER JOIN Vehicles ON Orders.Vehicle_ID = Vehicles.ID_Vehicle
+INNER JOIN Models ON Vehicles.Model_ID = Models.ID_Model
+INNER JOIN Brands ON Vehicles.Brand_ID = Brands.ID_Brand;
